78eb4440fe7438435ea7c53d9b7dd1180bf4ca1d
Alexander Korzun <xcloudx@mail.ru>
2013-06-13 01:58:25 +0300

    Добавлена LeftHandMenu

---
+++ Sources/Client.Web/Controllers/Controls/Menu/LeftMenuControlController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using Client.Web.Models.Controls.Menu;
+using Common.Security;
+
+namespace Client.Web.Controllers.Controls.Menu
+{
+    [AllowAnonymous]
+    public class LeftMenuControlController : BaseController<LeftMenuControlController>
+    {
+        public const string LeftMenuControlPath = "Controls/Menu/LeftMenuControl";
+
+        [ChildActionOnly]
+        public PartialViewResult Index()
+        {
+            LeftMenuModel model = CreateModel();
+
+            UpdateEnabledModules(model);
+
+            return PartialView(LeftMenuControlPath, model);
+        }
+
+        protected virtual LeftMenuModel CreateModel()
+        {
+            return new LeftMenuModel();
+        }
+
+        protected virtual void UpdateEnabledModules(LeftMenuModel model)
+        {
+            model.HomeUrl = Url.Action(IndexAction, HomeController.Name);
+            model.IsMyAccountEnabled = UserPrincipal.IsAuthenticated;
+        }
+
+    }
+}
---
+++ Sources/Client.Web/Models/Controls/Menu/LeftMenuModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Web.Models.Controls.Menu
+{
+    public class LeftMenuModel
+    {
+        public string HomeUrl { get; set; }
+
+        public bool IsMyAccountEnabled { get; set; }
+    }
+}
